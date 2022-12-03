a27fd4f0a79a872abc1f79ea9da7cf30ce6d2dad
king <kingsharpn@gmail.com>
2022-12-03 15:39:51 +0000

    server update

---
+++ server/routes/routes.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const Model = require('../models/model');
+
+const router = express.Router()
+
+//Post Method
+router.post('/savenote', (req, res) => {
+    // res.send('Post API')
+     if (!req.body) {
+        res.status(400).send({message:"content can not be empty!"})
+        return;
+    }
+    const data = new Model({
+        title: req.body.title,
+        body: req.body.body
+    })
+
+    data.save()
+    .then(data =>{
+        res.json(data)
+        console.log('data save');
+        
+    })
+    .catch(err =>{
+        console.log(err);
+        res.json(err)
+    })
+})
+
+//Get all Method
+router.get('/getAll', (req, res) => {
+    // res.send('Get All API')
+     Model.find()
+    .then((result) =>{
+        res.send(result)
+    })
+    .catch((err) =>{
+        console.log(err);
+    })
+})
+
+//Get by ID Method
+router.get('/getOne/:id', (req, res) => {
+    res.send('Get by ID API')
+})
+
+//Update by ID Method
+router.patch('/update/:id', (req, res) => {
+    res.send('Update by ID API')
+})
+
+//Delete by ID Method
+router.delete('/delete/:id', (req, res) => {
+    res.send('Delete by ID API')
+})
+
+module.exports = router;
